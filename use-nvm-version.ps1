5e3c9b6db1fbc09d11040a6cebc8dc444fe68630
Alja Čekada <alja.cekada@gmail.com>
2024-01-09 16:52:55 -0800

    Add custom script to use specific node version

--- use-nvm-version.ps1
+++ use-nvm-version.ps1
@@ -1,11 +1,3 @@
 $version = Get-Content .\.nvmrc
 
-# Check if the version is installed
-$installedVersions = nvm ls
-if ($installedVersions -notcontains $version) {
-    # If not installed, install it
-    nvm install $version
-}
-
-# Use the version
 nvm use $version
